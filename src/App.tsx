5dbf6426e3b5dded35a58be3e2e9dcd483d2f0ac
ling.huang <ling.huang@convertlab.com>
2022-02-07 20:12:41 -0800

    feat: update book mark page list

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,99 @@
 import { useEffect, useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Bookmark, BookmarkFolder, BookmarkUrl } from 'type';
 import { decryptData, getPasswordHash } from 'utils';
-import { Bookmark } from 'type';
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0);
+export default function App() {
+  const [syncId, setSyncId] = useState('');
+  const [password, setPassword] = useState('');
+  const [bookmarkItems, setBookmarkItems] = useState<Bookmark[]>([]);
+  const [loading, setLoading] = useState(false);
 
-  useEffect(() => {
-    async function load() {
-      const syncId = 'xxx';
+  async function load(syncId: string, passwordHash: string) {
+    setLoading(true);
+    try {
       const { bookmarks } = await (
         await fetch(`https://api.xbrowsersync.org/bookmarks/${syncId}`)
       ).json();
-      console.log(bookmarks);
-      const passwordHash = await getPasswordHash('xxx', syncId);
-      const data = await decryptData(bookmarks, passwordHash);
-      const bookmarkItems: Bookmark[] = JSON.parse(data);
 
-      console.log(bookmarkItems);
+      const data = await decryptData(bookmarks, passwordHash);
+      const bookmarkItems: [{ children: Bookmark[] }] = JSON.parse(data);
+
+      setBookmarkItems(bookmarkItems[0]?.children || []);
+    } catch (e) {
+      // do nothing
     }
-    load();
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    const syncId = localStorage.getItem('syncId');
+    const passwordHash = localStorage.getItem('password');
+    if (!syncId || !passwordHash) {
+      return;
+    }
+    setSyncId(syncId);
+    load(syncId, passwordHash);
   }, []);
 
+  async function onSave() {
+    const passwordHash = await getPasswordHash(password, syncId);
+    localStorage.setItem('password', passwordHash);
+    localStorage.setItem('syncId', syncId);
+    load(syncId, passwordHash);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div>
+      <div>
+        <span>syncId:</span>
+        <input value={syncId} onChange={(e) => setSyncId(e.target.value)} />
+      </div>
+      <div>
+        <span>password:</span>
+        <input value={password} onChange={(e) => setPassword(e.target.value)} />
+      </div>
+      <button disabled={!syncId || !password || loading} onClick={onSave}>
+        Save
+      </button>
+      {bookmarkItems && <BookmarkItems items={bookmarkItems} />}
     </div>
   );
 }
 
-export default App;
+function BookmarkItems({ items }: { items: Bookmark[] }) {
+  return (
+    <>
+      {items.map((item) => {
+        if ((item as BookmarkFolder).children) {
+          return <Folder item={item as BookmarkFolder} />;
+        } else {
+          return (
+            <div className="item">
+              <Link {...(item as BookmarkUrl)} />
+            </div>
+          );
+        }
+      })}
+    </>
+  );
+}
+
+const Link = ({ url, title }: BookmarkUrl) => (
+  <a href={url} target="_blank" rel="noreferrer">
+    {title}
+  </a>
+);
+
+const Folder = ({ item }: { item: BookmarkFolder }) => {
+  const [collapsed, setCollapsed] = useState(false);
+  return (
+    <div className="folder">
+      <div onClick={() => setCollapsed(!collapsed)}>
+        <span className="handler" data-collapsed={collapsed ? 'true' : 'false'} />
+        <span>{item.title}</span>
+      </div>
+      {collapsed ? null : <BookmarkItems items={item.children} />}
+    </div>
+  );
+};
--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [tsconfigPaths(), react()],
+  server: {
+    open: '/index.html',
+    https: true,
+  },
 });
